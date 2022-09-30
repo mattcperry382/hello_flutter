279d09f5b7237e0dd9215405fc80c25f7847dd86
mattcperry382 <mattcperry382@gmail.com>
2022-09-29 18:36:45 -0600

    formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import './screens/home.dart';
 
-void main() => runApp(HelloFlutterApp());
+void main() => runApp(
+      HelloFlutterApp(),
+    );
 
 class HelloFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: "Hello Flutter App",
-    home: Scaffold(
+      title: "Hello Flutter App",
+      home: Scaffold(
         appBar: AppBar(
           title: const Text("Title in App Bar"),
         ),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,15 +4,15 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-            color: Colors.deepPurple,
-            child: Center(
-            child: Text(
-              sayHello(),
-              textDirection: TextDirection.ltr,
-              style: const TextStyle(color: Colors.white, fontSize: 36.0),
-            )
-          )
-        );
+      color: Colors.deepPurple,
+      child: Center(
+        child: Text(
+          sayHello(),
+          textDirection: TextDirection.ltr,
+          style: const TextStyle(color: Colors.white, fontSize: 36.0),
+        ),
+      ),
+    );
   }
 
   String sayHello() {
@@ -22,14 +22,13 @@ class Home extends StatelessWidget {
     int minute = now.minute;
     if (hour < 12) {
       hello = "Good morning";
-    }
-    else if (hour < 18 ) {
+    } else if (hour < 18) {
       hello = "Good afternoon";
-    }
-    else {
+    } else {
       hello = "Good evening";
     }
-    String minutes = (minute < 10) ? "0${minute.toString()}" : minute.toString();
+    String minutes =
+        (minute < 10) ? "0${minute.toString()}" : minute.toString();
 
     return "It's now ${hour.toString()}:${minute.toString()}.\n$hello";
   }
